f41839e0ccc6002c2f2356f6b643ab0f988f2637
Abdulrahman Ghasemlou <89246514+SARS-COV-2-Mpro@users.noreply.github.com>
2025-12-13 16:41:15 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const NEXT_PROXY = 'https://healthy-whale-82.deno.dev';
+
+app.all('/*', async (req, res) => {
+    try {
+        const response = await axios({
+            method: req.method,
+            url: NEXT_PROXY + req.path,
+            params: req.query,
+            headers: {
+                'x-mbx-apikey': req.headers['x-mbx-apikey'] || ''
+            }
+        });
+        res.json(response.data);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+app.get('/', (req, res) => res.send('Koyeb Proxy Alive'));
+
+app.listen(8000);
